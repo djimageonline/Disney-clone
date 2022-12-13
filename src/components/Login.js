479e2e3ad7b2bd66142dc913d680f76c63a1ce48
Johnny Proano <djimageonline@gmail.com>
2022-12-13 01:35:24 -0500

    center content and create container for main login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,24 @@
 import styled from "styled-components";
 
 const Login = (props) => {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <Content>Content</Content>
+    </Container>
+  );
 };
+
+//This will be outside container of main disney page
+// This will also center the content below
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+
+//This will be the content inside the container of disney page
+const Content = styled.div``;
 
 export default Login;
