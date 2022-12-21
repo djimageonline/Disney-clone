011c57c43092bbad799438c7a769062ff8d6d75b
Johnny Proano <djimageonline@gmail.com>
2022-12-21 14:55:37 -0500

    added PLay, Trailer, and plus functionality to details page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -7,6 +7,22 @@ const Detail = (props) => {
         <img src="" alt="" />
       </Background>
       <ImageTitle></ImageTitle>
+      <ContentMeta>
+        <Controls>
+          <Player>
+            <img src="/images/play-icon-black.png" alt="" />
+            <span>Play</span>
+          </Player>
+          <Trailer>
+            <img src="/images/play-icon-white.png" alt="" />
+            <span>Trailer</span>
+          </Trailer>
+          <AddList>
+            <span></span>
+            <span></span>
+          </AddList>
+        </Controls>
+      </ContentMeta>
     </Container>
   );
 };
@@ -55,4 +71,96 @@ const ImageTitle = styled.div`
     width: 35vw;
   }
 `;
+
+const ContentMeta = styled.div`
+  max-width: 874px;
+`;
+
+const Controls = styled.div`
+  align-items: center;
+  display: flex;
+  flew-flow: row nowrap;
+  margin: 24px 0px;
+  min-height: 56px;
+`;
+
+//Play button style
+const Player = styled.button`
+  font-size: 15px;
+  margin: 0px 22px 0px 0px;
+  padding: 0px 24px;
+  height 56px;
+  border-radius: 4px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: 1.8px;
+  text-align: center;
+  text-transformation: uppercase;
+  background: rgb (249, 249, 249);
+  border: none;
+  color: rgb(0, 0, 0);
+
+  img {
+    width: 32px;
+  }
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+
+  @media (max-width: 768px) {
+    height: 45px;
+    padding: 0px 12px;
+    font-size: 12px;
+    margin: 0px 10px 0px 0px;
+
+    img {
+      width: 25px;
+    }
+
+  }
+
+`;
+
+// Trailer button style
+const Trailer = styled(Player)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+`;
+
+const AddList = styled.div`
+  margin-right: 16px;
+  height: 44px;
+  width: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.6);
+
+  border-radius: 50%;
+  border: 2px solid white;
+  cursor: pointer;
+
+  // Plus sign in AddList
+  span {
+    background-color: rgb(249, 249, 249);
+    display: inline-block;
+
+    &:first-child {
+      height: 2px;
+      transform: translate(1px, 0px) rotate(0deg);
+      width: 16px;
+    }
+
+    &:nth-child(2) {
+      height: 16px;
+      transform: translateX(-8px) rotate(0deg);
+      width: 2px;
+    }
+  }
+`;
+
 export default Detail;
